26d279c540c92b5ba9cd6828a0df086a46f4e33b
Claire Carroll <carroll.claire.e@gmail.com>
2019-07-08 17:10:07 -0400

    Use second argument in generate_schema_name

--- starter-project/macros/get_custom_schema.sql
+++ starter-project/macros/get_custom_schema.sql
@@ -1,5 +1,5 @@
 {% raw -%}
-{% macro generate_schema_name(schema_name) -%}
-    {{ generate_schema_name_for_env(schema_name) }}
+{% macro generate_schema_name(schema_name, node) -%}
+    {{ generate_schema_name_for_env(schema_name, node) }}
 {%- endmacro %}
 {%- endraw %}
