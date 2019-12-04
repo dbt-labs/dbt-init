9c411ea418f71ffe9169db151b4ce493b07af310
Jeremy Cohen <jeremy@fishtownanalytics.com>
2019-12-04 17:27:11 -0500

    Use DRYer Jinja

--- starter-project/macros/grant_select_on_schemas.sql
+++ starter-project/macros/grant_select_on_schemas.sql
@@ -1,11 +1,12 @@
 {% if project.warehouse in ('postgres', 'redshift') %}
 {% raw %}
 {% macro grant_select_on_schemas(schemas, groups) %}
+  {% set groups_csv = 'group' ~  groups | join(', group ') %}
   {% for schema in schemas %}
-    grant usage on schema {{ schema }} to group {{ groups|join(', group ') }};
-    grant select on all tables in schema {{ schema }} to group {{ groups|join(', group ') }};
+    grant usage on schema {{ schema }} to group {{ groups_csv }};
+    grant select on all tables in schema {{ schema }} to group {{ groups_csv }};
     alter default privileges in schema {{ schema }}
-        grant select on tables to group {{ groups|join(', group ') }};
+        grant select on tables to group {{ groups_csv }};
   {% endfor %}
 {% endmacro %}
 {% endraw %}
