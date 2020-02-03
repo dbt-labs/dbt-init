150fc07a69ebb039b13de48a1b56567a7316c67a
Claire Carroll <carroll.claire.e@gmail.com>
2020-02-03 12:36:04 -0500

    Make sure this query gets run

--- starter-project/macros/grant_select_on_database_operation.sql
+++ starter-project/macros/grant_select_on_database_operation.sql
@@ -1,6 +1,8 @@
 {% if project.warehouse == 'snowflake' %}
 {% raw %}
 {% macro grant_select_on_database(role, database=target.database) %}
+
+{% set grant_sql %}
     grant usage on database analytics to {{ role }};
 
     grant usage on all schemas in database {{ database }} to role {{ role }};
@@ -10,6 +12,14 @@
 
     grant select on future views in database {{ database }} to role {{ role }};
     grant select on all tables in database {{ database }} to role {{ role }};
+{% endset %}
+
+{{ dbt_utils.log_info("Granting read privileges on all relations in " ~ database ~ " to " ~ role) }}
+
+{% do run_query(grant_sql) %}
+
+{{ dbt_utils.log_info("Privileges granted") }}
+
 {% endmacro %}
 {% endraw %}
 {% endif %}
